f33560f53ab4aec350011d946969bc21be1e9ec0
_MoscoviumQuark <moscoviumquark@gmail.com>
2025-11-28 12:58:37 -0600

    Update ModBlocks.java

--- Forge/src/main/java/net/moscoviumquark/dripstonedecor/block/ModBlocks.java
+++ Forge/src/main/java/net/moscoviumquark/dripstonedecor/block/ModBlocks.java
@@ -18,52 +18,58 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, DripstoneDecor.MOD_ID);
 
     public static final RegistryObject<Block> DRIPSTONE_STAIRS = registerBlock("dripstone_stairs",
-            () -> new StairBlock(Blocks.DRIPSTONE_BLOCK::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.DRIPSTONE_BLOCK)));
+            () -> new StairBlock(
+                    Blocks.DRIPSTONE_BLOCK.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.DRIPSTONE_BLOCK)));
     public static final RegistryObject<Block> DRIPSTONE_SLAB = registerBlock("dripstone_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.DRIPSTONE_BLOCK)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DRIPSTONE_BLOCK)));
     public static final RegistryObject<Block> DRIPSTONE_WALL = registerBlock("dripstone_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.DRIPSTONE_BLOCK)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DRIPSTONE_BLOCK)));
 
     public static final RegistryObject<Block> CALCITE_STAIRS = registerBlock("calcite_stairs",
-            () -> new StairBlock(Blocks.CALCITE::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.CALCITE)));
+            () -> new StairBlock(
+                    Blocks.CALCITE.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE)));
     public static final RegistryObject<Block> CALCITE_SLAB = registerBlock("calcite_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.CALCITE)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE)));
     public static final RegistryObject<Block> CALCITE_WALL = registerBlock("calcite_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.CALCITE)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE)));
 
     public static final RegistryObject<Block> SMOOTH_BASALT_STAIRS = registerBlock("smooth_basalt_stairs",
-            () -> new StairBlock(Blocks.SMOOTH_BASALT::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.SMOOTH_BASALT)));
+            () -> new StairBlock(
+                    Blocks.SMOOTH_BASALT.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.SMOOTH_BASALT)));
     public static final RegistryObject<Block> SMOOTH_BASALT_SLAB = registerBlock("smooth_basalt_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.SMOOTH_BASALT)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SMOOTH_BASALT)));
     public static final RegistryObject<Block> SMOOTH_BASALT_WALL = registerBlock("smooth_basalt_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.SMOOTH_BASALT)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SMOOTH_BASALT)));
 
     public static final RegistryObject<Block> GILDED_BLACKSTONE_STAIRS = registerBlock("gilded_blackstone_stairs",
-            () -> new StairBlock(Blocks.GILDED_BLACKSTONE::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.GILDED_BLACKSTONE)));
+            () -> new StairBlock(
+                    Blocks.GILDED_BLACKSTONE.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.GILDED_BLACKSTONE)));
     public static final RegistryObject<Block> GILDED_BLACKSTONE_SLAB = registerBlock("gilded_blackstone_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.GILDED_BLACKSTONE)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.GILDED_BLACKSTONE)));
     public static final RegistryObject<Block> GILDED_BLACKSTONE_WALL = registerBlock("gilded_blackstone_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.GILDED_BLACKSTONE)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.GILDED_BLACKSTONE)));
 
     public static final RegistryObject<Block> END_STONE_STAIRS = registerBlock("end_stone_stairs",
-            () -> new StairBlock(Blocks.END_STONE::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.END_STONE)));
+            () -> new StairBlock(
+                    Blocks.END_STONE.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE)));
     public static final RegistryObject<Block> END_STONE_SLAB = registerBlock("end_stone_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE)));
     public static final RegistryObject<Block> END_STONE_WALL = registerBlock("end_stone_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE)));
 
     public static final RegistryObject<Block> PACKED_MUD_STAIRS = registerBlock("packed_mud_stairs",
-            () -> new StairBlock(Blocks.PACKED_MUD::defaultBlockState,
-                    BlockBehaviour.Properties.copy(Blocks.PACKED_MUD)));
+            () -> new StairBlock(
+                    Blocks.PACKED_MUD.defaultBlockState(),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.PACKED_MUD)));
     public static final RegistryObject<Block> PACKED_MUD_SLAB = registerBlock("packed_mud_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.PACKED_MUD)));
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PACKED_MUD)));
     public static final RegistryObject<Block> PACKED_MUD_WALL = registerBlock("packed_mud_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.PACKED_MUD)));
+            () -> new WallBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PACKED_MUD)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
